af80939e27dff1d3ee5dff693827563e18092a29
Steve Losh <steve@stevelosh.com>
2012-09-20 11:29:49 -0400

    Smarter volunteering.

--- matchers/volunteer.py
+++ matchers/volunteer.py
@@ -3,15 +3,36 @@ from base import BaseMatcher
 
 
 class VolunteerMatcher(BaseMatcher):
-    dev_text = "volunteer someone"
-    all_text = "volunteer a dev"
+    all_text = "volunteer someone"
+    all_text_other = "volunteer someone else"
+    dev_text = "volunteer a dev"
+    dev_text_other = "volunteer another dev"
+
     dev_candidates = ['sjl', 'arthurdebert', 'honza', 'fernandotakai', 'nicksergeant']
     all_candidates = dev_candidates + ['cz', 'ehazlett']
 
-    def respond(self, message, user=None):
+    def choose(self, message, user):
+        victim = None
+
+        if self.dev_text_other in message.lower():
+            while (not victim) or victim == user:
+                victim = random.choice(self.dev_candidates)
+            return victim
+
         if self.dev_text in message.lower():
-            victim = random.choice(self.dev_candidates)
+            return random.choice(self.dev_candidates)
+
+        if self.all_text_other in message.lower():
+            while (not victim) or victim == user:
+                victim = random.choice(self.all_candidates)
+            return victim
+
+        if self.all_text in message.lower():
+            return random.choice(self.all_candidates)
+
+    def respond(self, message, user=None):
+        victim = self.choose(message, user)
+
+        if victim:
             self.speak('%s is it' % victim)
-        elif self.all_text in message.lower():
-            victim = random.choice(self.all_candidates)
-            self.speak('%s is it' % victim)
+
