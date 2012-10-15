a8f7fd1801e345500f47409c0da16bf780786855
Honza Pokorny <me@honza.ca>
2012-10-15 13:58:11 -0300

    Herp derp

--- matchers/lunch.py
+++ matchers/lunch.py
@@ -6,6 +6,6 @@ class LunchMatcher(BaseMatcher):
     name = 'lunch'
 
     def respond(self, message, user=None):
-        if (len(message) < 10) and ('lunch' in message.lower()):
+        if (len(message) < 20) and ('lunch' in message.lower()):
             message = user + ': enjoy'
             self.speak(message)
