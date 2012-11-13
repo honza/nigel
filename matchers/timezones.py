57f75224cb716069d6a87e383ee64534f6445486
Arthur Debert <arthur@stimuli.com.br>
2012-11-13 18:02:26 -0200

    Actually show the correct timezones

--- matchers/timezones.py
+++ matchers/timezones.py
@@ -2,18 +2,18 @@ import datetime
 from base import BaseMatcher
 from pytz import timezone
 import pytz
-
+import re
 class TimezoneMatcher(BaseMatcher):
 
     name = 'timezone'
     greetings = [
         'timezone',
-        ''
     ]
     timezones = {
         'EST':timezone('US/Eastern'),
         'Sao Paulo': timezone('America/Sao_Paulo'),
         'San Francisco': timezone('America/Tijuana'),
+        'UTC': timezone('Etc/UTC')
     }
     fmt = '%H:%M:%S %Z%z'
     
@@ -21,11 +21,29 @@ class TimezoneMatcher(BaseMatcher):
         if user and user.startswith('unisubs-jenkins'):
             return
 
-        if not message.startswith('timezone'):
+        if not message.startswith('timezone') and not message.startswith('tz'):
             return
-        now  = datetime.datetime.now()
+        convert_match = re.match('tz\s+(?P<hour>[\d]{1,2})\s+(?P<timezone_name>[\w]+)', message)
+        now = datetime.datetime.utcnow()
+        if convert_match:
+            match_dict =  convert_match.groupdict()
+            if match_dict['timezone_name'] not in self.timezones.keys():
+                self.speak("Timezone '%s' not found, valid ones are %s" % (",".join(self.timezones.keys())))
+                return
+            tz_info = self.timezones[match_dict['timezone_name']]
+            # FIZME: create with the correct timezone, dammit
+            base_date = datetime.datetime(year=now.year,
+                                          month=now.month,
+                                          day=now.day,
+                                          hour=convert_match.groupdict()['hour'],
+                                          tzinfo=tz_info)
+        else:
+            base_date = datetime.datetime(year=now.year, month=now.month,
+                                          day=now.day, hour=now.hour,
+                                          minute=now.minute, tzinfo=pytz.utc)
+            
         buffer = []
         for name,tz in self.timezones.items():
-            buffer.append("%s : %s" % (name, tz.localize(now)))
+            buffer.append("%s : %s" % (name, base_date.astimezone(tz)))
         message = str("\n".join(buffer))
         self.speak(message)
